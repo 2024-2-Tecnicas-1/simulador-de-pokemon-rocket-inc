f5a73a281874ca0d73019b8b4a622ada5eb8b7dd
jyrjrhjry <yruyruryuru4@gmail.com>
2024-10-28 08:30:48 -0500

    Se agrego el enum TipoPokemon

--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -1,5 +1,53 @@
 package simulador.pokemon;
 
-public class TipoPokemon {
-    // TODO: Aquí va tu código
+public enum TipoPokemon {
+    FUEGO,
+    AGUA,
+    PLANTA,
+    VENENO,
+    ELECTRICO,
+    PSIQUICO,
+    ROCA,
+    TIERRA,
+    VOLADOR,
+    NORMAL,
+    LUCHA,
+    ACERO,
+    BICHO,
+    HIELO
 }
+public 
+
+double class calcularFactorMultiplicador 
+
+    (TipoPokemon atacante, TipoPokemon defensor) {
+        switch (atacante) {
+        case FUEGO:
+            switch (defensor) {
+                case AGUA:
+                case ACERO:
+                case FUEGO:
+                    return 0.5;
+                case PLANTA:
+                case ACERO:
+                case BICHO:
+                case HIELO:
+                    return 2.0;
+                default:
+                    return 1.0;
+            }
+        case AGUA:
+            switch (defensor){
+                case AGUA:
+                case PLANTA:
+                    return 0.5;
+                case FUEGO:
+                case ROCA:
+                case TIERRA;
+                return 2.0
+                        default:
+                            return 1.0;
+            }
+    }
+}
+       
