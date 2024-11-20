74d49f39a3c807556f09955fe5f13f9910f257d3
Ronaldinho <jersrodriguez0@gmail.com>
2024-11-19 23:44:18 -0500

    Correciones en growlithe

--- src/main/java/BusinessLogic/Growlithe.java
+++ src/main/java/BusinessLogic/Growlithe.java
@@ -1,47 +1,48 @@
 package BusinessLogic;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Growlithe extends Pokemon {
 
-    private final Ataque[] ataques; 
+    private final List<Ataque> ataques; 
 
     public Growlithe(String nombre) {
         super(nombre, TipoPokemon.FUEGO, 55, 70);
-        ataques = new Ataque[3];
+        ataques = new ArrayList<>();
         cargarAtaques(); 
     }
 
     private void cargarAtaques() {
- 
         Ataque llamarada = new Ataque("Llamarada", TipoPokemon.FUEGO, 50, EstadoPokemon.QUEMADO, 0.3);
         Ataque gruñido = new Ataque("Gruñido", TipoPokemon.NORMAL, 0, EstadoPokemon.CONFUSO, 0.2);
         Ataque mordisco = new Ataque("Mordisco", TipoPokemon.FUEGO, 40, EstadoPokemon.NORMAL, 0.1);
 
-        ataques[0] = llamarada;
-        ataques[1] = gruñido;
-        ataques[2] = mordisco;
+        ataques.add(llamarada);
+        ataques.add(gruñido);
+        ataques.add(mordisco);
     }
 
     @Override
     public void atacar(int indiceAtaque, Pokemon defensor) {
- 
-        if (indiceAtaque < 0 || indiceAtaque >= ataques.length) {
+        if (indiceAtaque < 0 || indiceAtaque >= ataques.size()) {
             System.out.println("Índice de ataque no válido.");
             return; 
         }
 
-        Ataque ataqueSeleccionado = ataques[indiceAtaque];
+        Ataque ataqueSeleccionado = ataques.get(indiceAtaque);
         ataqueSeleccionado.aplicarAtaque(defensor);
     }
 
     @Override
     public void entrenar() {
-
     }
 
-    public Ataque[] getAtaques() {
+    public List<Ataque> getAtaques() {
         return ataques;
     }
 }
+
 
 
 
